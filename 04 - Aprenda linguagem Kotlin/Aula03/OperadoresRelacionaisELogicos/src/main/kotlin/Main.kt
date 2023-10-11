1cb4254a98790e358913f54f2f0f84b46a4b38da
luksinocencio <luksinocencio@gmail.com>
2023-10-11 11:05:23 -0300

    feat: operadores relacionais e logicos

---
+++ 04 - Aprenda linguagem Kotlin/Aula03/OperadoresRelacionaisELogicos/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+/**
+ * > que representa maior que
+ * < que representa menor que
+ * >= que representa maior ou igual
+ * <= que representa menor ou igual
+ * == que representa igual
+ * != que representa diferente
+ * */
+    val numero1 = 10
+    val numero2 = 10
+
+    print(numero1 >= numero2)
+}

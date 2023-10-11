909426b81d958e5ff5269614d1505d7d6b1e1a02
luksinocencio <luksinocencio@gmail.com>
2023-10-11 11:03:54 -0300

    feat: variaveis e constantes

---
+++ 04 - Aprenda linguagem Kotlin/Aula01/VariablesConstants/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+const val welcomeMessage = "Welcome"
+
+fun main(args: Array<String>) {
+    val nome: String = "Lucas"
+    var sobrenome: String
+    sobrenome = "Inocencio"
+    print("${welcomeMessage} ${nome} ${sobrenome}!")
+}

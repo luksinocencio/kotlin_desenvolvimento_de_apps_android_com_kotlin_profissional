196f1ac0435ec3fc2113ba819efcc77d8d1e6c2f
luksinocencio <luksinocencio@gmail.com>
2023-10-11 11:04:31 -0300

    feat: operadores aritmeticos

---
+++ 04 - Aprenda linguagem Kotlin/Aula02/OperadoresAritimeticos/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val numero1 = 11
+    val numero2 = 5
+
+    val resultadoSoma = numero1 + numero2
+    val resultadoSubtracao = numero1 - numero2
+    val resultadoMultiplicacao = numero1 * numero2
+    val resultadoDivisao = numero1 * numero2
+    val resultadoRestoDaDivisao = numero1 * numero2
+
+    println(resultadoSoma)
+    println(resultadoSubtracao)
+    println(resultadoMultiplicacao)
+    println(resultadoDivisao)
+    println(resultadoRestoDaDivisao)
+}

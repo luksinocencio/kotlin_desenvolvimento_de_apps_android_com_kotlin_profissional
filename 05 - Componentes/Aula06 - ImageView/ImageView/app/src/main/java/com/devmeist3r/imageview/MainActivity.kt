0046e52313a2f01cfa9cf4556a692d2c01e059a5
luksinocencio <luksinocencio@gmail.com>
2023-10-12 11:29:09 -0300

    feat: imageView

---
+++ 05 - Componentes/Aula06 - ImageView/ImageView/app/src/main/java/com/devmeist3r/imageview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.devmeist3r.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devmeist3r.imageview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.luffy5)
+        }
+    }
+}

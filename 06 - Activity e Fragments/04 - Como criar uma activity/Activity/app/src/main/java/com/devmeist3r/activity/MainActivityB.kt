9088073aeea6a71e73a55457649a4368b0a494ae
luksinocencio <luksinocencio@gmail.com>
2023-10-12 13:29:24 -0300

    feat: como criar uma empty activity

---
+++ 06 - Activity e Fragments/04 - Como criar uma activity/Activity/app/src/main/java/com/devmeist3r/activity/MainActivityB.kt
@@ -0,0 +1,11 @@
+package com.devmeist3r.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_b)
+    }
+}

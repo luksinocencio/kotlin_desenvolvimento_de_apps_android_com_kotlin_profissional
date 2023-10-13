9a269e1896d14ab8b029e96aa84aa301b39da47b
luksinocencio <luksinocencio@gmail.com>
2023-10-13 01:08:13 -0300

    feat: navegando entre fragments

---
+++ 06 - Activity e Fragments/11 - Navegando entre fragments/Fragments/app/src/main/java/com/devmeist3r/fragments/FragmentA.kt
@@ -0,0 +1,40 @@
+package com.devmeist3r.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.devmeist3r.fragments.databinding.FragmentABinding
+
+class FragmentA : Fragment() {
+    private var _binding: FragmentABinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentABinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentA_to_fragmentB)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

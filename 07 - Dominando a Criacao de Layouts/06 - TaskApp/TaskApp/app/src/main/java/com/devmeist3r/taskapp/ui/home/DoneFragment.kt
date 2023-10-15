55f76fbd84c13a13e6aaa5442af58fc7b9784e59
luksinocencio <luksinocencio@gmail.com>
2023-10-15 17:40:07 -0300

    feat: tela de status tablayout

---
+++ 07 - Dominando a Criacao de Layouts/06 - TaskApp/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/home/DoneFragment.kt
@@ -0,0 +1,29 @@
+package com.devmeist3r.taskapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.devmeist3r.taskapp.R
+import com.devmeist3r.taskapp.databinding.FragmentDoingBinding
+import com.devmeist3r.taskapp.databinding.FragmentDoneBinding
+
+class DoneFragment : Fragment() {
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

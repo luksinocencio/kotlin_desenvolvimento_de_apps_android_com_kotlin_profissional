e45072e65f57232dadd1c090ef9878dcee944762
luksinocencio <luksinocencio@gmail.com>
2023-10-15 14:09:19 -0300

    feat: criando navegacao padrao

--- 07 - Dominando a Criacao de Layouts/06 - TaskApp/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/auth/LoginFragment.kt
+++ 07 - Dominando a Criacao de Layouts/06 - TaskApp/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/auth/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.devmeist3r.taskapp.R
 import com.devmeist3r.taskapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -18,6 +20,21 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
     }
 
     override fun onDestroyView() {

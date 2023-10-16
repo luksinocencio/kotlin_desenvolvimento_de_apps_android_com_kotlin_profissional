25edb910295b129df282de519cee87490657c73c
luksinocencio <luksinocencio@gmail.com>
2023-10-16 00:12:11 -0300

    feat: tela formTask e todo fragment

--- 07 - Dominando a Criacao de Layouts/06 - TaskApp/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/home/TodoFragment.kt
+++ 07 - Dominando a Criacao de Layouts/06 - TaskApp/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/home/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.devmeist3r.taskapp.R
 import com.devmeist3r.taskapp.databinding.FragmentDoneBinding
 import com.devmeist3r.taskapp.databinding.FragmentTodoBinding
@@ -20,6 +21,17 @@ class TodoFragment : Fragment() {
     ): View {
         _binding = FragmentTodoBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
     }
 
     override fun onDestroyView() {

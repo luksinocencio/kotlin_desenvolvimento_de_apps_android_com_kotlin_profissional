350dc52187aa758f9a7d1f6122ec8216fac93282
luksinocencio <luksinocencio@gmail.com>
2023-10-19 02:01:09 -0300

    feat: criacao layout item recycler view

---
+++ 08 - Aprendendo Sobre Listagem de Dados/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,63 @@
+package com.devmeist3r.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.devmeist3r.taskapp.R
+import com.devmeist3r.taskapp.databinding.FragmentLoginBinding
+import com.devmeist3r.taskapp.util.showBottomSheet
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnLogin.setOnClickListener {
+            validateData()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+    private fun validateData() {
+        val email = binding.editEmail.text.toString().trim()
+        val password = binding.editPassword.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                findNavController().navigate(R.id.action_global_homeFragment)
+            } else {
+                showBottomSheet(message = getString(R.string.password_empty))
+            }
+        } else {
+            showBottomSheet(message = getString(R.string.email_empty))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ 08 - Aprendendo Sobre Listagem de Dados/TaskApp/app/src/main/java/com/devmeist3r/taskapp/ui/home/TodoFragment.kt
@@ -0,0 +1,40 @@
+package com.devmeist3r.taskapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.devmeist3r.taskapp.R
+import com.devmeist3r.taskapp.databinding.FragmentTodoBinding
+
+class TodoFragment : Fragment() {
+    private var _binding: FragmentTodoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTodoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

2c3959974685a142fff79e1c0914cf4ed4f0d934
luksinocencio <luksinocencio@gmail.com>
2023-11-20 19:17:52 -0300

    feat: verificar login no app

--- 13 - Banco digital/BancoApp/app/src/main/java/br/com/devmeist3r/bancoapp/presenter/splash/SplashFragment.kt
+++ 13 - Banco digital/BancoApp/app/src/main/java/br/com/devmeist3r/bancoapp/presenter/splash/SplashFragment.kt
@@ -1,12 +1,16 @@
 package br.com.devmeist3r.bancoapp.presenter.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.devmeist3r.bancoapp.R
 import br.com.devmeist3r.bancoapp.databinding.FragmentSplashBinding
+import br.com.devmeist3r.bancoapp.util.FirebaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,8 +28,21 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::verifyAuth, 3000)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun verifyAuth() {
+        if (FirebaseHelper.isAuthenticated()) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_navigation)
+        }
     }
 }
---
+++ 13 - Banco digital/BancoApp/app/src/main/java/br/com/devmeist3r/bancoapp/util/FirebaseHelper.kt
@@ -0,0 +1,35 @@
+package br.com.devmeist3r.bancoapp.util
+
+import com.google.firebase.auth.FirebaseAuth
+
+class FirebaseHelper {
+    companion object {
+        fun isAuthenticated() = FirebaseAuth.getInstance().currentUser != null
+//        fun getDatabase() = Firebase.database.reference
+//        fun getAuth() = FirebaseAuth.getInstance()
+//        fun getIdUser() = getAuth().currentUser?.uid ?: ""
+//        fun isAutenticated() = getAuth().currentUser != null
+//        fun validError(error: String): Int {
+//            return when {
+//                error.contains("There is no user record corresponding to this identifier") -> {
+//                    R.string.account_not_registered_register_fragment
+//                }
+//                error.contains("The email address is badly formatted") -> {
+//                    R.string.invalid_email_register_fragment
+//                }
+//                error.contains("The password is invalid or the user does not have a password") -> {
+//                    R.string.invalid_password_register_fragment
+//                }
+//                error.contains("The email address is already in use by another account") -> {
+//                    R.string.email_in_use_register_fragment
+//                }
+//                error.contains("Password should be at least 6 characters") -> {
+//                    R.string.strong_password_register_fragment
+//                }
+//                else -> {
+//                    R.string.error_generic
+//                }
+//            }
+//        }
+    }
+}

656e542e581d7730c21222142220a95871ca4460
luksinocencio <luksinocencio@gmail.com>
2023-11-22 18:57:17 -0300

    feat: inserir recoverUser

--- 13 - Banco digital/BancoApp/app/src/main/java/br/com/devmeist3r/bancoapp/presenter/auth/recover/RecoverFragment.kt
+++ 13 - Banco digital/BancoApp/app/src/main/java/br/com/devmeist3r/bancoapp/presenter/auth/recover/RecoverFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import br.com.devmeist3r.bancoapp.R
 import br.com.devmeist3r.bancoapp.databinding.FragmentRecoverBinding
+import br.com.devmeist3r.bancoapp.util.StateView
 import br.com.devmeist3r.bancoapp.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class RecoverFragment : Fragment() {
     private var _binding: FragmentRecoverBinding? = null
     private val binding get() = _binding!!
+    private val recoverViewModel: RecoverViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,8 +45,29 @@ class RecoverFragment : Fragment() {
 
         if (email.isNotEmpty()) {
             Toast.makeText(requireContext(), "E-mail enviado!", Toast.LENGTH_SHORT).show()
+            recoverUser(email)
         } else {
             Toast.makeText(requireContext(), "Digite seu e-mail", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun recoverUser(email: String) {
+        recoverViewModel.recover(email).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.circularProgressIndicator.isVisible = true
+                }
+
+                is StateView.Sucess -> {
+                    binding.circularProgressIndicator.isVisible = false
+                    Toast.makeText(requireContext(), "E-mail enviado com sucesso", Toast.LENGTH_SHORT).show()
+                }
+
+                is StateView.Error -> {
+                    binding.circularProgressIndicator.isVisible = false
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
